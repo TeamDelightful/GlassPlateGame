787fd138f0e873a58414e639b845cf98ea5d7f89
Kristin <kristin@kristinbell.org>
2021-11-01 03:30:55 -0700

    deleting and modifying files that are no longer needed

--- routes/creategame.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET creategame page. */
-router.get('/', function(req, res, next) {
-  res.render('creategame');
-});
-
-module.exports = router;
